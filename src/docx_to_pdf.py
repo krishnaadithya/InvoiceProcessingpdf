3c8c18ec26d7c60e74c7efefda0d2d33c25e358d
Krishna Adithya <krishnaadithya@Krishnas-MacBook-Air.local>
2025-03-07 02:32:56 -0800

    updated code

--- src/docx_to_pdf.py
+++ src/docx_to_pdf.py
@@ -5,15 +5,16 @@ def docx_to_pdf(input_file, output_file=None):
     #convert doc,docx into pdf
     # Ensure LibreOffice is installed and get absolute path
     input_path = os.path.abspath(input_file)
-    output_dir = os.path.dirname(input_path)  # Save in the same directory
-
+    output_dir = os.path.dirname(output_file)
     # Run LibreOffice command
     command = ["libreoffice", "--headless", "--convert-to", "pdf", input_path, "--outdir", output_dir]
     subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Return output file path
-    if output_file is None: 
-      output_file = os.path.join(output_dir, os.path.splitext(os.path.basename(input_file))[0] + ".pdf")
+    output_file_path = os.path.join(output_dir, os.path.basename(input_file).replace('.docx', '.pdf'))
+
+    #rename the output file to the original file name
+    os.rename(output_file_path, output_file)
+
     return output_file
 
 
